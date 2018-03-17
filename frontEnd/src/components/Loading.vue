ae872b8230842cc7b1c2da4e6bc3637996a3f119
cyril.alvarez <cyril@2getherbank.com>
2018-03-17 18:53:17 +0100

    fix

--- frontEnd/src/components/Loading.vue
+++ frontEnd/src/components/Loading.vue
@@ -1,13 +1,5 @@
 <template>
-    <v-card class="white justify-center">
-        <v-container fluid>
-            <v-layout row align-center>
-                <v-flex xs12>
-                    <v-progress-circular indeterminate :size="70" :width="7" color="purple"></v-progress-circular>
-                </v-flex>
-            </v-layout>
-        </v-container>
-    </v-card>
+    <v-progress-circular indeterminate :size="70" :width="7" color="purple"></v-progress-circular>        
 </template>
 <script>
 
